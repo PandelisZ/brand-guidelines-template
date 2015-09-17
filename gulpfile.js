6716e855bf2895ee6c7c14b7e667aacab2f87903
Paul Best <apbest@gmail.com>
2015-09-17 11:39:57 -0400

    Make a generic starter client

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,7 @@
+
+/* ========================================================================= */
+// Add Dependencies
+// -------------
 var gulp        = require('gulp'),
 
     // Server and sync
@@ -9,6 +13,7 @@ var gulp        = require('gulp'),
     es          = require('event-stream'),
     sass        = require('gulp-sass'),
     jade        = require('gulp-jade'),
+    marked      = require('marked'), // For :markdown filter in jade
     sourcemaps  = require('gulp-sourcemaps'),
     minifyCss   = require('gulp-minify-css'),
     rename      = require('gulp-rename'),
@@ -16,11 +21,17 @@ var gulp        = require('gulp'),
     fs          = require('fs.extra');
 
 
+
+
+/* ========================================================================= */
+// Serve Web Files
+// -------------
+
 // Server initiation and livereload, opens server in browser
 gulp.task('serve', function() {
   browserSync.init(null, {
     server: {
-      baseDir: './dist',
+      baseDir: './build',
       index: 'index.html'
     },
     open: "external",
@@ -29,125 +40,195 @@ gulp.task('serve', function() {
 });
 
 
+
+/* ========================================================================= */
+// Style
+// -------------
+
+//
+// Config
+//
+var styleConfig = {
+  paths: {
+    src: './build/app/scss/main.scss',
+    dest: './build/app/css/'
+  },
+  watch: ['./build/app/scss/**/*.scss']
+};
+
+//
 // SASS compiling & reloading
+//
 gulp.task('sass', function() {
-    gulp.src('./src/bin/scss/main.scss')
+    gulp.src(styleConfig.paths.src)
       .pipe(plumber())
-      .pipe(sourcemaps.init())
       .pipe(sass({
         errLogToConsole: true
       }))
-      .pipe(gulp.dest('./dist/css/'))
-      .pipe(minifyCss({compatibility: 'ie8'}))
-      .pipe(sourcemaps.write())
-      .pipe(rename({suffix: '.min'}))
-      .pipe(gulp.dest('./dist/css/'))
+      .pipe(gulp.dest(styleConfig.paths.dest))
+      .pipe(gulp.dest(styleConfig.paths.dest))
       .pipe(browserSync.reload({
         stream: true
       }));
 });
 
 //
-// Compile Our HTML
-//
-gulp.task('jade', function() {
-  gulp.src('./src/index.jade')
-    .pipe(jade({
-      pretty: true
-    }))
-    .pipe(gulp.dest('./dist/'))
-    .pipe(browserSync.reload({
-      stream: true
-    }));
-});
-
-//
-// Compile Email Signature
-//
-gulp.task('create-email-signature', function() {
-  gulp.src('./src/bin/jade/email-signature.jade')
-    .pipe(jade({
-      pretty: true
-    }))
-    .pipe(gulp.dest('./src/brand-files/email-signature/'))
-    .pipe(browserSync.reload({
-      stream: true
-    }));
-});
-
-//
-// Move Brand files into dist
-//
-gulp.task('move-brand-files', function(){
-  rimraf('./dist/brand-files', cb);
-  fs.copyRecursive('./src/brand-files', './dist/brand-files', function (err) {
-    if (err) {
-      throw err;
-    }
-    console.log("Copied './src/brand-files' to './dist/brand-files'");
-  });
-});
-
-//
-// Clear 'dist' directory, then minifying, copying, processing, uglifying, etc for build
-//
-gulp.task('remove', function (cb) {
-    rimraf('./dist', cb);
-});
-
-//
 // Minify sass
 //
-gulp.task('minify', ['sass'], function() {
-  return gulp.src('./dist/css/*.css')
+gulp.task('optimizeCSS', ['sass'], function() {
+  return gulp.src(styleConfig.paths.src)
+    .pipe(plumber())
     .pipe(minifyCSS({
       keepSpecialComments: 0
     }))
     .pipe(gulp.dest('./dist/css'));
 });
 
+/* ========================================================================= */
+// JADE
+// -------------
+
+//
+// Config
+//
+var jadeConfig = {
+  paths: {
+    src: './build/index.jade',
+    dest: './build/'
+  },
+  watch: ['./build/index.jade', './build/guidelines/*.jade', './build/app/jade/*.jade']
+};
+
+//
+// Compile Our HTML
+//
+gulp.task('jade', function() {
+  gulp.src(jadeConfig.paths.src)
+    .pipe(plumber())
+    .pipe(jade({
+      pretty: true
+    }))
+    .pipe(gulp.dest(jadeConfig.paths.dest))
+    .pipe(browserSync.reload({
+      stream: true
+    }));
+});
+
+
+
+/* ========================================================================= */
+// JADE - Email Signature
+// -------------
+
+//
+// Config
+//
+var emailSigConfig = {
+  paths: {
+    src: './build/app/jade/email-signature.jade',
+    dest: './build/brand-files/email-signature/'
+  },
+  watch: ['./build/brand-files/email-signature/variables.jade','./build/app/jade/email-signature.jade']
+};
+
+//
+// Compile Email Signature
+//
+gulp.task('create-email-signature', function() {
+  gulp.src(emailSigConfig.paths.src)
+    .pipe(jade({
+      pretty: true
+    }))
+    .pipe(gulp.dest(emailSigConfig.paths.dest))
+    .pipe(browserSync.reload({
+      stream: true
+    }));
+});
+
+
+
+/* ========================================================================= */
+// Scripts
+// -------------
+
+//
+// Config
+//
+var scriptConfig = {
+  paths: {
+    src: '*.scss',
+    dest: '*.js'
+  },
+  watch: ['./build/app/js/**/*.js']
+};
 
 //
 // Uglify js
 //
 gulp.task('scripts', function() {
-    return gulp.src('./src/bin/js/*.js')
-        //.pipe(uglify())
-        .pipe(gulp.dest('./dist/js'))
+    return gulp.src(scriptConfig.paths.src)
+        .pipe(plumber())
+        .pipe(gulp.dest(scriptConfig.paths.src))
         .pipe(browserSync.reload({
           stream: true,
           once: true
         }));
 });
 
-//
-// Watching files for changes before reloading
-//
-gulp.task('watch-js', function() {
-  gulp.src('./dist/**/*.js')
-      .pipe(browserSync.reload({
-        stream: true,
-        once: true
-      }));
+gulp.task('optimizeJS', ['scripts'], function() {
+    return gulp.src(scriptConfig.paths.src)
+        .pipe(plumber())
+        .pipe(uglify())
+        .pipe(gulp.dest(scriptConfig.paths.src))
 });
 
-//
-// Default functionality includes server with browser sync and watching
-//
-gulp.task('default', ['remove','sass', 'scripts', 'jade', 'create-email-signature', 'move-brand-files', 'serve'], function(){
-  gulp.watch(['./src/index.jade', './src/guidelines/*.jade', './src/bin/jade/*.jade'], ['jade']);
-  gulp.watch('./src/bin/scss/**/*.scss', ['sass']);
-  gulp.watch('./src/brand-files/email-signature/email-signature.jade', ['create-email-signature']);
-  gulp.watch('./src/bin/js/**/*.js', ['scripts']);
-  gulp.watch(['./src/brand-files/**/*','./src/brand-files/*'], ['move-brand-files']);
-});
+
+/* ========================================================================= */
+// Package for Distribution
+// -------------
 
 //
 // Build functionality with cleaning, moving, compiling, etc.
 //
 gulp.task('build', ['remove'], function(){
   return gulp.start(
-    'minify',
-    'scripts'
+    'optimizeCSS',
+    'optimizeJS',
+    'moveFiles'
   );
 });
+
+//
+// Remove 'dist' directory, then minifying, copying, processing, uglifying, etc for build
+//
+gulp.task('remove', function (cb) {
+    rimraf('./dist/', cb);
+});
+
+//
+// Move Files into dist
+//
+gulp.task('move-files', function(cb){
+   fs.copyRecursive('./build', './dist', function (err) {
+    if (err) {
+      throw err;
+    }
+    });
+ });
+
+
+
+/* ========================================================================= */
+// Default Task
+// -------------
+
+//
+// Default functionality includes server with browser sync and watching
+//
+gulp.task('default', ['create-email-signature','sass', 'scripts', 'jade', 'serve'], function(){
+  gulp.watch(jadeConfig.watch, ['jade']);
+  gulp.watch(styleConfig.watch, ['sass']);
+  gulp.watch(scriptConfig.watch, ['scripts']);
+  gulp.watch(emailSigConfig.watch, ['create-email-signature']);
+});
+
